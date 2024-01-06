8e53ca1a4e9a052fcf149d05ae57fc6490c1410c
Krunal Ninghot <kdninghot520@gmail.com>
2024-01-07 02:22:12 +0530

    Third

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -75,7 +75,7 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <img className='img1' src='http://www.hindigraphics.in/wp-content/uploads/2019/01/pro.png' alt='' />
+      <img className='img1' src={'http://www.hindigraphics.in/wp-content/uploads/2019/01/pro.png'} alt='' />
       <form className="login-form" onSubmit={handleSubmit}>
         <input
           type="email"
